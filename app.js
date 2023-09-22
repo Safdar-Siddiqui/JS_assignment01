c864acf7c376c6ff334cdafe0512588cbce8867b
Safdar Siddiqui <safdarsiddiqui25@gmail.com>
2023-09-22 20:37:41 +0500

    assignment01 file

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Welcome to my First JS Webpage :)");
+
+alert("Error! Please enter a valid password");
+
+alert("Welcome to JS Land... \nHappy Coding!");
+
+alert("Welcome to JS Land...");
+
+alert("Happy Coding!\n Prevent this page from creating additional dialogs");
+
+alert("Hello... I can run JS through my Web Brower's console");
+
+console.log("Hello... I can run JS through my Web Brower's console");
